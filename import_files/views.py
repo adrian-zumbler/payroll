f4fa1cd8370d22cc7cfc782fa915226ea97f66cf
Adrian <adrianm1@outlook.com>
2015-08-31 10:13:45 -0500

    Cambio

--- import_files/views.py
+++ import_files/views.py
@@ -6,13 +6,10 @@ class ImportView(View):
 
     def get(self,request):
         if request.user.is_authenticated():
-<<<<<<< HEAD
-			return render(request, 'importFiles/import.html')
-=======
+
             if request.user.is_staff:
                 return render(request, 'importFiles/import.html')
             else:
                 return redirect(settings.LOGIN_URL)
->>>>>>> 961c02ecd8d922af191c2f3074c25ff5c0219d89
         else:
 			return redirect('%s?next=%s' % (settings.LOGIN_URL,request.path))
