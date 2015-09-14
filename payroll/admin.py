7f21d64b34ec261a11d30d00f33390464ac84788
Adrian Meza <adrianm1@outlook.com>
2015-09-14 14:32:42 -0600

    Se agrego la opcion de exporte

--- payroll/admin.py
+++ payroll/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Payroll
 
 class PayrollAdmin(admin.ModelAdmin):
-    list_display = ('date',)
+    list_display = ['date', 'getAgentFullName']
+
+    def getAgentFullName(self,obj):
+        return '%s %s' % (obj.agent.first_name, obj.agent.last_name)
 
 admin.site.register(Payroll,PayrollAdmin)
