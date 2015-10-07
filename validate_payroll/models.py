609e33dce23d659ade19f17cc2905e024dc5b40a
Adrian Meza <adrianm1@outlook.com>
2015-10-07 08:02:38 -0600

    Added date and name description in the object validate_payroll

--- validate_payroll/models.py
+++ validate_payroll/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import User
 class ValidatePayroll(models.Model):
     date = models.DateField()
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return ('%s %s') % (self.date,self.user.username)
