337852768ce1e99ef0cbab37ccdddd33e2ae60a2
Adrian Meza <adrianm1@outlook.com>
2016-01-07 00:58:13 -0700

    added atributes to the model restriction_files model

--- restriction_files/models.py
+++ restriction_files/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class RestricionFile(models.Model):
-    file = models.FileField(upload_to="restricion_files")
+    file = models.FileField(upload_to="restriction_files")
